20ebd85fdf25cb3c7c6bfed637ac9935a158eeb6
htmujahid <htmujahid@gmail.com>
2023-08-24 11:21:27 +0500

    23-08-2023

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -14,11 +14,7 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    const { slug } = body;
-
-    return NextResponse.json({
-        req
-    });    
+    const { slug } = body; 
     
     const matters = await createPost(slug);
 
